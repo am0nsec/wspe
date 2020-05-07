a1781234c6c97f712ad71b901e81e037cc3d1045
Paul Laîné (@am0nsec) <20334678+am0nsec@users.noreply.github.com>
2020-05-08 00:49:14 +0100

    Update and rename main.c to list_objects.c

--- Windows Objects/list_objects.c
+++ Windows Objects/list_objects.c
@@ -1,3 +1,12 @@
+/*+===================================================================
+  File:      list_objects.c
+  Summary:   List objects from a Windows Object Manager directory.
+  Classes:   N/A
+  Functions: N/A
+  Origin:    https://github.com/am0nsec
+##
+  Author: Paul Laine (@am0nsec)
+===================================================================+*/
 #pragma once
 #include <Windows.h>
 #include <stdio.h>
@@ -12,7 +21,6 @@ VOID RtlInitUnicodeString(
 	_In_ PUNICODE_STRING DestinationString,
 	_In_ PCWSTR          SourceString
 );
-
 NTSTATUS WINAPI NtOpenDirectoryObject(
 	_Out_ PHANDLE            DirectoryHandle,
 	_In_  ACCESS_MASK        DesiredAccess,
@@ -28,23 +36,11 @@ NTSTATUS WINAPI NtQueryDirectoryObject(
 	_Out_opt_ PULONG  ReturnLength
 );
 
-BOOL OpenRootUserDirectoryObject(
-	_Out_ PHANDLE hHandle,
-	_In_  PWCHAR pRootDirectory
-);
-
-/*--------------------------------------------------------------------
-  Global variables.
---------------------------------------------------------------------*/
-UNICODE_STRING g_CurrentDirectory = { 0 };
-UNICODE_STRING g_RootUsrDirectory = { 0 };
-HANDLE         g_hProcessHeap     = NULL;
-
 INT wmain(INT argc, PWCHAR argv[]) {
-	wprintf(L"----------------------------------------------------\n");
-	wprintf(L"    List objects from the Windows Object Manager\n");
-	wprintf(L"        Copyright (C) Paul Laine (@amonsec)\n");
-	wprintf(L"----------------------------------------------------\n\n");
+	wprintf(L"------------------------------------------------------\n");
+	wprintf(L" List objects from a Windows Object Manager directory\n");
+	wprintf(L"         Copyright (C) Paul Laine (@amonsec)\n");
+	wprintf(L"------------------------------------------------------\n\n");
 
 	/*--------------------------------------------------------------------
 	  Check user input.
@@ -52,21 +48,33 @@ INT wmain(INT argc, PWCHAR argv[]) {
 	if (argc == 1) {
 		wprintf(L"[-] Usage: \n");
 		wprintf(L"    - WindowsObjects.exe <directory> \n");
-		wprintf(L"    - WindowsObjects.exe \\ \n\n");
 		wprintf(L"[-] Examples: \n");
+		wprintf(L"    - WindowsObjects.exe \\ \n");
 		wprintf(L"    - WindowsObjects.exe \\ObjectTypes \n");
 		wprintf(L"    - WindowsObjects.exe \\Sessions\\1\\BaseNamedObjects \n\n");
 		return 0x1;
 	}
-	RtlInitUnicodeString(&g_RootUsrDirectory, argv[1]);
-	RtlInitUnicodeString(&g_CurrentDirectory, argv[1]);
-	g_hProcessHeap = GetProcessHeap();
-
+	
 	/*--------------------------------------------------------------------
 	  Get a handle to the directory object.
 	--------------------------------------------------------------------*/
+	UNICODE_STRING RootDirectory = { 0 };
+	RtlInitUnicodeString(&RootDirectory, argv[1]);
+
 	HANDLE hRootDirectory = NULL;
-	if (!OpenRootUserDirectoryObject(&hRootDirectory, L"\\ObjectTypes")) {
+	HANDLE hProcessHeap = GetProcessHeap();
+
+	OBJECT_ATTRIBUTES ObjAttributes;
+	ObjAttributes.Attributes = OBJ_CASE_INSENSITIVE;
+	ObjAttributes.Length = sizeof(OBJECT_ATTRIBUTES);
+	ObjAttributes.ObjectName = &RootDirectory;
+	ObjAttributes.RootDirectory = NULL;
+	ObjAttributes.SecurityDescriptor = NULL;
+	ObjAttributes.SecurityQualityOfService = NULL;
+
+	NTSTATUS status = NtOpenDirectoryObject(&hRootDirectory, DIRECTORY_QUERY, &ObjAttributes);
+	if (!NT_SUCCESS(status) || hRootDirectory == INVALID_HANDLE_VALUE) {
+		wprintf(L"[-] Error invoking ntdll!NtOpenDirectoryObject (0x%08x)\n", (DWORD)status);
 		return 0x1;
 	}
 
@@ -76,9 +84,8 @@ INT wmain(INT argc, PWCHAR argv[]) {
 	POBJECT_DIRECTORY_INFORMATION pObjInformation = NULL;
 	ULONG uStructureSize = 0;
 	ULONG uContext = 0;
-	NTSTATUS status;
 
-	wprintf(L"%+-15ws %+-20ws %ws\n", L"Directory", L"TypeName", L"Name");
+	wprintf(L" %+-32ws %ws\n\n", L"Type", L"Name");
 	do {
 		// Get length of the structure
 		status = NtQueryDirectoryObject(hRootDirectory, NULL, 0, TRUE, FALSE, &uContext, &uStructureSize);
@@ -92,7 +99,7 @@ INT wmain(INT argc, PWCHAR argv[]) {
 		}
 
 		// Get the information about the object
-		pObjInformation = HeapAlloc(g_hProcessHeap, HEAP_ZERO_MEMORY, uStructureSize);
+		pObjInformation = HeapAlloc(hProcessHeap, HEAP_ZERO_MEMORY, uStructureSize);
 		status = NtQueryDirectoryObject(hRootDirectory, pObjInformation, uStructureSize, TRUE, FALSE, &uContext, NULL);
 		if (!NT_SUCCESS(status)) {
 			wprintf(L"[-] Error invoking ntdll!NtQueryDirectoryObject (0x%08x)\n", (DWORD)status);
@@ -100,33 +107,13 @@ INT wmain(INT argc, PWCHAR argv[]) {
 			return 0x1;
 		}
 
-		wprintf(L"%+-15ws %+-20ws %ws\n", g_RootUsrDirectory.Buffer, pObjInformation->TypeName.Buffer, pObjInformation->Name.Buffer);
-
-		// Cleanup
+		wprintf(L" %+-32ws %ws\n", pObjInformation->TypeName.Buffer, pObjInformation->Name.Buffer);
+		HeapFree(hProcessHeap, 0, pObjInformation);
 		uStructureSize = 0;
-		HeapFree(g_hProcessHeap, 0, pObjInformation);
 	} while (status != STATUS_NO_MORE_ENTRIES);
 	
 	// Cleanup and exit
 	if (hRootDirectory)
 		CloseHandle(hRootDirectory);
 	return 0x0;
-}
-
-BOOL OpenRootUserDirectoryObject(PHANDLE pHandle) {
-	OBJECT_ATTRIBUTES ObjAttributes;
-	ObjAttributes.Attributes = OBJ_CASE_INSENSITIVE;
-	ObjAttributes.Length = sizeof(OBJECT_ATTRIBUTES);
-	ObjAttributes.ObjectName = &g_RootUsrDirectory;
-	ObjAttributes.RootDirectory = NULL;
-	ObjAttributes.SecurityDescriptor = NULL;
-	ObjAttributes.SecurityQualityOfService = NULL;
-
-	NTSTATUS nt = NtOpenDirectoryObject(pHandle, DIRECTORY_QUERY, &ObjAttributes);
-	if (!NT_SUCCESS(nt) || pHandle == INVALID_HANDLE_VALUE) {
-		wprintf(L"Error invoking ntdll!NtOpenDirectoryObject (0x%08x)\n", (DWORD64)nt);
-		return FALSE;
-	}
-
-	return TRUE;
 }
