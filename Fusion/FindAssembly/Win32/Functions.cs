8c02dfd8b72d781035f8bd2eee0b23d7b31994f2
am0nsec <20334678+am0nsec@users.noreply.github.com>
2020-10-06 14:55:43 +0100

    fix missing files

---
+++ Fusion/FindAssembly/Win32/Functions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FindAssembly.Fusion;
+using System.Runtime.InteropServices;
+
+namespace FindAssembly.Win32 {
+    internal class Functions {
+
+        [DllImport("fusion.dll", CharSet = CharSet.Auto)]
+        [PreserveSig]
+        public static extern uint CreateAssemblyEnum(
+            out IAssemblyEnum ppEnum,
+            IntPtr pUnkReserved,
+            IAssemblyName pName,
+            ASM_CACHE_FLAGS dwFlags,
+            IntPtr pvReserved
+        );
+
+        [DllImport("fusion.dll", CharSet = CharSet.Auto)]
+        [PreserveSig]
+        public static extern uint CreateAssemblyCache(
+            out IAssemblyCache ppAsmCache,
+            uint dwReserved
+        );
+    }
+}
---
+++ Fusion/FindAssembly/Win32/Macros.cs
@@ -0,0 +1,11 @@
+﻿
+namespace FindAssembly.Win32 {
+    public static class Macros {
+
+        public static bool SUCCEEDED(uint hr) => hr >= S_OK;
+
+        public static uint S_OK = 0x00000000;
+        public static uint E_FAIL = 0x80004005;
+
+    }
+}
